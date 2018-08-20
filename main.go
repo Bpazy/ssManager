05bf939fe182037a83382abb8dbfc1176e8ac259
ziyuan <hanziyuan08@outlook.com>
2018-08-20 20:52:19 +0800

    remove render

--- main.go
+++ main.go
@@ -1,33 +1,35 @@
 package main
 
 import (
-	"github.com/Bpazy/ssManager/user"
+	"bufio"
+	"bytes"
 	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os/exec"
 )
 
 const ok = http.StatusOK
 const failed = http.StatusInternalServerError
 
 func main() {
-	engine := gin.Default()
-
-	engine.Use(user.DefaultAuth())
-	engine.Any(user.DefaultLoginUrl, user.Login())
-
-	engine.LoadHTMLGlob("templates/*")
-	engine.Static("/assets", "./assets")
-	engine.Any("/", defaultHandler)
-
-	engine.Any("/findSsConfig", findConfigHandler)
-	engine.Any("/addPortPassword", addPortPasswordHandler)
-	engine.Run(config.Addr)
+	//engine := gin.Default()
+	//
+	//engine.Use(user.DefaultAuth())
+	//engine.Any(user.DefaultLoginUrl, user.Login())
+	//
+	//engine.Any("/", defaultHandler)
+	//
+	//engine.Any("/findSsConfig", findConfigHandler)
+	//engine.Any("/addPortPassword", addPortPasswordHandler)
+	//engine.Run(config.Addr)
+	test()
 }
 
 func defaultHandler(c *gin.Context) {
-	c.HTML(200, "index.html", gin.H{
-		"title": "tttt",
-	})
+	c.JSON(ok, "")
 }
 
 func addPortPasswordHandler(c *gin.Context) {
@@ -54,4 +56,27 @@ func findConfigHandler(c *gin.Context) {
 		return
 	}
 	c.JSON(ok, ssConfig)
+}
+
+func test() {
+	cmd := exec.Command("bash", "-c", "iptables -")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 保证关闭输出流
+	defer stdout.Close()
+	// 运行命令
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	// 读取输出结果
+	opBytes, err := ioutil.ReadAll(stdout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(bytes.NewReader(opBytes))
+	for scanner.Scan() {
+		log.Println(scanner.Text())
+	}
 }
