eeff7476c9f6c0ee8a7c2dd0168e9275f43b8d5d
ziyuan <onbpazy@163.com>
2018-09-08 01:02:10 +0800

    renew

--- main.go
+++ main.go
@@ -1,57 +1,5 @@
 package main
 
-import (
-	"github.com/Bpazy/ssManager/user"
-	"github.com/gin-gonic/gin"
-	"net/http"
-)
-
-const ok = http.StatusOK
-const failed = http.StatusInternalServerError
-
 func main() {
-	engine := gin.Default()
 
-	engine.Use(user.DefaultAuth())
-	engine.Any(user.DefaultLoginUrl, user.Login())
-
-	engine.LoadHTMLGlob("templates/*")
-	engine.Static("/assets", "./assets")
-	engine.Any("/", defaultHandler)
-
-	engine.Any("/findSsConfig", findConfigHandler)
-	engine.Any("/addPortPassword", addPortPasswordHandler)
-	engine.Run(config.Addr)
-}
-
-func defaultHandler(c *gin.Context) {
-	c.HTML(200, "index.html", gin.H{
-		"title": "tttt",
-	})
-}
-
-func addPortPasswordHandler(c *gin.Context) {
-	ssConfig, err := findSsConfig()
-	if err != nil {
-		c.JSON(failed, err)
-		return
-	}
-	port, _ := c.GetQuery("port")
-	password, _ := c.GetQuery("password")
-	ssConfig.PortPassword[port] = password
-	err = saveSsConfig(ssConfig)
-	if err != nil {
-		c.JSON(failed, err)
-		return
-	}
-	c.JSON(ok, "add success.")
-}
-
-func findConfigHandler(c *gin.Context) {
-	ssConfig, err := findSsConfig()
-	if err != nil {
-		c.JSON(failed, err)
-		return
-	}
-	c.JSON(ok, ssConfig)
 }
